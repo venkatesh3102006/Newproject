93d68fd64c01fbe93523f33e49305dc0b5a41513
Venkateshwaran A <24cse.venkateshwaran.a@skct.edu.in>
2026-03-12 22:36:08 +0530

    update backend url to student-base-management-2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://student-base-management-1.onrender.com/api',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://student-base-management-2.onrender.com/api',
   },
 }
 
